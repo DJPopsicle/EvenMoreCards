a03b4ff3fea1f132381d7e3549bcde498dec1280
DJ Popsicle <djpopsicle.music@gmail.com>
2021-10-20 15:35:52 -0700

    Add files

---
+++ EvenMoreCards/EvenMoreCards.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using EvenMoreCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace EvenMoreCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class EvenMoreCards : BaseUnityPlugin
+    {
+        private const string ModId = "com.My.Mod.Id";
+        private const string ModName = "MyModName";
+        public const string ModInitials = "EMC";
+        public static EvenMoreCards instance { get; private set; }
+        public const string Version = "0.0.0"; // What version are we on (major.minor.patch)?
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            CustomCard.BuildCard<Magazine>();
+            instance = this;
+        }
+    }
+}
